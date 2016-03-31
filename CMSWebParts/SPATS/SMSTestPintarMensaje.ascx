3de24d7eedf1a5f50dd8c48ef9c1ca530564102b
GladysSanchez <gladys.sanchez@iica.int>
2016-03-31 12:45:30 -0500

    Creacion de WebPart para pintar mensaje

---
+++ CMSWebParts/SPATS/SMSTestPintarMensaje.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="SMSTestPintarMensaje.ascx.cs" Inherits="CMSWebParts_SPATS_SMSTestPintarMensaje" %>
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+<asp:Button ID="BtnIngresar" runat="server" CssClass="PnlBtn" 
+    onclick="Button1_Click" Text="Ingresar" />
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
---
+++ CMSWebParts/SPATS/SMSTestPintarMensaje.ascx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+using CMS.PortalControls;
+using CMS.GlobalHelper;
+using SMSNotification;
+using CMS.SettingsProvider;
+using CMS.CMSHelper;
+
+using CMS.DatabaseHelper;
+using CMS.DataEngine;
+using CMS.SettingsProvider;
+
+using CMS.EventLog;
+using CMS.Scheduler;
+using CMS.SiteProvider;
+
+
+public partial class CMSWebParts_SPATS_SMSTestPintarMensaje : CMSAbstractWebPart
+
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = TextBox1.Text;
+        TextBox1.Text = "";
+
+    }
+}
